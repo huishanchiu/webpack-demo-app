3421b438f032ebf07b959e814d6124624f407f54
joychiuu <joy.chiu@urmart.com>
2024-10-17 11:43:49 +0800

    Add loader to handel css

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,13 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"), // 打包後的檔案應該儲存的位置，__dirname:當前檔案所在的目錄
   },
+  // loader:webpack 只理解 JavaScript 和 JSON 檔。loader允許 webpack 處理其他類型的檔
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], // 先執行css-loader把結尾是.css 的檔案轉成JS,style-loader負責把此JS注入DOM
+      },
+    ],
+  },
 };
