40ca81ddae2e5f3fc4bf6e82507172ca4b9a4f06
joychiuu <jolley0901@gmail.com>
2024-10-22 09:43:14 +0800

    Add Sass loader and override boostrap color
    -Change main.css to main.scss
    -Installed boostrap locally
    -Installed sass-loader
    -Add .scss rule on webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,17 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"), // 打包後的檔案應該儲存的位置，__dirname:當前檔案所在的目錄
   },
+  // loader:webpack 只理解 JavaScript 和 JSON 檔。loader允許 webpack 處理其他類型的檔案
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. Injects styles to tje DOM
+          "css-loader", // 2. Turns css into JS
+          "sass-loader", // 1, Turns sass into css
+        ],
+      },
+    ],
+  },
 };
