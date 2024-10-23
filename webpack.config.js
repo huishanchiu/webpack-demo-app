27514142843ff6275ff3c14c064b157ba32d5be9
joychiuu <joy.chiu@urmart.com>
2024-10-24 07:15:11 +0800

    Remove cache and install HtmlWebpackPlugin
    -Used contentHash in bundle file name
    -Installed HtmlWebpackPlugin to generate a new index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Template } = require("webpack");
+
 module.exports = {
   mode: "development", // 此模式打包出來的 main.js 依然是好閱讀的
-  entry: "/src/index.js", // 告訴 webpack 要從哪開始打包
+  entry: "./src/index.js", // 告訴 webpack 要從哪開始打包
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html", // webpack 幫我們生成 html
+    }),
+  ],
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js", // 在檔案有更新時會生成不同名字的 js 防止瀏覽器快取
     path: path.resolve(__dirname, "dist"), // 打包後的檔案應該儲存的位置，__dirname:當前檔案所在的目錄
   },
   // loader:webpack 只理解 JavaScript 和 JSON 檔。loader允許 webpack 處理其他類型的檔案
