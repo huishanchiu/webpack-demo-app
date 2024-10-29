863dfbb04c06183910adf44ea14f05f6f2dc3534
joychiuu <joy.chiu@urmart.com>
2024-10-30 07:38:00 +0800

    Add prod and dev configs, add dev-server -Split webpack.config to prod, dev and common -Installed webpack-merge to share the common functionality -Installed webpack-dev-server and added it to start script in package.json

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { Template } = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js", // 告訴 webpack 要從哪開始打包
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html", // webpack 幫我們生成 html
+    }),
+  ],
+  // loader:webpack 只理解 JavaScript 和 JSON 檔。loader允許 webpack 處理其他類型的檔案
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // 3. Injects styles to tje DOM
+          "css-loader", // 2. Turns css into JS
+          "sass-loader", // 1, Turns sass into css
+        ],
+      },
+    ],
+  },
+};
---
+++ webpack.prod.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(common, {
+  mode: "production",
+  output: {
+    filename: "main.[contentHash].js", // 在檔案有更新時會生成不同名字的 js 防止瀏覽器快取
+    path: path.resolve(__dirname, "dist"), // 打包後的檔案應該儲存的位置，__dirname:當前檔案所在的目錄
+  },
+});
